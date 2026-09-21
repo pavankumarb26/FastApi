e9bd70657cb74efc7a2ece5f183e84a1c17e36c5
pavankumarb26 <boddepallipavankumar2@gmail.com>
2026-09-21 07:40:16 +0530

    pproducts

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from models import Product
 
 app = FastAPI()
 
@@ -7,8 +8,11 @@ app = FastAPI()
 def greet():
     return "Welcome to Telusko Trac"
 
-
+products = [
+    Product(1,"phone","budget phone",99,10),
+    Product(2,"laptop","gaming laptop",999,6)
+]
 
 @app.get("/products")
 def get_products():
-    return "products"
+    return products
--- models.py
+++ models.py
@@ -4,11 +4,9 @@ class Product:
     descripition:str
     price:float
     quantity: int
-    def init_(self, id: int, name: str, description: str, price: float, quantity: int):
+    def __init__(self, id: int, name: str, description: str, price: float, quantity: int):
         self.id = id
         self.name = name
         self.description =  description
         self.price = price
         self.quantity = quantity
-
-I
